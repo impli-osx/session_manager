60da34d74c83fc8ed2f79a3f1863e13871d72403
impli <mrimpli@gmail.com>
2024-01-10 16:29:10 +0100

    new_file:   config.json modified:   configuration.py

--- configuration.py
+++ configuration.py
@@ -7,19 +7,15 @@ class MainWindow(QMainWindow):
         super().__init__(parent)
         self.ui = Ui_Configuration()
         self.ui.setupUi(self)
-            
+        self.charger_conf()
+        self.ui.Enregistrer.clicked.connect(self.enregistrer_conf)   
         # Load data from the JSON file
         try:
             with open("config.json", "r") as f:
                 data = json.load(f)
         except FileNotFoundError:
                 data = {}
-        """  
-        # Fill the user interface fields with the data
-        self.ui.fermeture_session.setText(data.get("fermeture_session", ""))
-        self.ui.activation_fermeture.setChecked(data.get("activation_fermeture", False))
-        """
-        self.ui.Enregistrer.clicked.connect(self.enregistrer_conf)
+
                     
     def enregistrer_conf(self):
         try:
@@ -72,21 +68,57 @@ class MainWindow(QMainWindow):
                     "fermeture_popup" : self.ui.activation_fermeture.isChecked(),
                 }
             }
-            # Cette fonction est appelée lorsque l'utilisateur clique sur le bouton "Enregistrer"
-
-            # Récupérez les données de l'interface utilisateur
-            data = {
-                "fermeture_session": self.ui.fermeture_session.text(),
-                "activation_fermeture": self.ui.activation_fermeture.isChecked(),
-                # Ajoutez ici d'autres champs si nécessaire
-            }
 
             # Enregistrez les données dans un fichier JSON
             with open("config.json", "w") as f:
-                json.dump(data, f)
+                json.dump(config, f,indent=4)
         except Exception as e:
             QMessageBox.critical(self, "Erreur", str(e))
             
+        self.close()
+            
+    def charger_conf(self):
+        try:
+            with open("config.json", "r") as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            data = {}
+            
+        self.ui.titre_popup_1.setText(data.get("titre", {}).get("titre_popup_1", ""))
+        self.ui.titre_popup_2.setText(data.get("titre", {}).get("titre_popup_2", ""))
+        self.ui.titre_popup_3.setText(data.get("titre", {}).get("titre_popup_3", ""))
+        self.ui.titre_popup_4.setText(data.get("titre", {}).get("titre_popup_4", ""))
+        self.ui.text_popup_1.setPlainText(data.get("text", {}).get("text_popup_1", ""))
+        self.ui.text_popup_2.setPlainText(data.get("text", {}).get("text_popup_2", ""))
+        self.ui.text_popup_3.setPlainText(data.get("text", {}).get("text_popup_3", ""))
+        self.ui.text_popup_4.setPlainText(data.get("text", {}).get("text_popup_4", ""))
+        self.ui.delai_fermeture.setText(data.get("timer", {}).get("delai_fermeture", ""))
+        self.ui.duree_session.setText(data.get("timer", {}).get("duree_session", ""))
+        self.ui.timer_second_popup.setText(data.get("timer", {}).get("timer_second_popup", ""))
+        self.ui.timer_troisieme_popup.setText(data.get("timer", {}).get("timer_troisieme_popup", ""))
+        self.ui.timer_dernier_popup.setText(data.get("timer", {}).get("timer_dernier_popup", ""))
+        self.ui.fiche_log.setChecked(data.get("fiche", {}).get("fiche_log", False))
+        self.ui.fiche_activation.setChecked(data.get("fiche", {}).get("fiche_activation", False))
+        self.ui.fiche_nom.setChecked(data.get("fiche", {}).get("fiche_nom", False))
+        self.ui.fiche_mail.setChecked(data.get("fiche", {}).get("fiche_mail", False))
+        self.ui.fiche_adresse.setChecked(data.get("fiche", {}).get("fiche_adresse", False))
+        self.ui.fiche_telephone.setChecked(data.get("fiche", {}).get("fiche_telephone", False))
+        self.ui.fiche_duree_session.setChecked(data.get("fiche", {}).get("fiche_duree_session", False))
+        self.ui.fiche_15min.setChecked(data.get("fiche", {}).get("fiche_15min", False))
+        self.ui.fiche_30min.setChecked(data.get("fiche", {}).get("fiche_30min", False))
+        self.ui.fiche_1h.setChecked(data.get("fiche", {}).get("fiche_1h", False))
+        self.ui.fiche_bouton_reglement.setChecked(data.get("fiche", {}).get("fiche_bouton_reglement", False))
+        self.ui.fiche_reglement.setChecked(data.get("fiche", {}).get("fiche_reglement", False))
+        self.ui.session_user.setCurrentText(data.get("session", {}).get("session_user", ""))
+        self.ui.session_activation.setChecked(data.get("session", {}).get("session_activation", False))
+        self.ui.largeur_popup.setText(data.get("style", {}).get("largeur_popup", ""))
+        self.ui.hauteur_popup.setText(data.get("style", {}).get("hauteur_popup", ""))
+        self.ui.police.setCurrentText(data.get("style", {}).get("police", ""))
+        self.ui.taille_police.setText(data.get("style", {}).get("taille_police", ""))
+        self.ui.fermeture_session.setCurrentText(data.get("fermeture", {}).get("fermeture_session", ""))
+        self.ui.activation_fermeture.setChecked(data.get("fermeture", {}).get("activation_fermeture", False))
+        
+        
 
 if __name__ == "__main__":
     app = QApplication([])
