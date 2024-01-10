db7f51f38b5e399a47d13b5160ab2b170b4c18a4
impli <mrimpli@gmail.com>
2024-01-10 14:48:39 +0100

    update

--- configuration.py
+++ configuration.py
@@ -19,9 +19,58 @@ class MainWindow(QMainWindow):
         self.ui.fermeture_session.setText(data.get("fermeture_session", ""))
         self.ui.activation_fermeture.setChecked(data.get("activation_fermeture", False))
         """
-        self.ui.Enregistrer.clicked.connect(self.on_save_clicked)
+        self.ui.Enregistrer.clicked.connect(self.enregistrer_conf)
                     
-    def on_save_clicked(self):
+    def enregistrer_conf(self):
+        try:
+            config = {
+                "titre" :{
+                    "titre_popup_1" : self.ui.titre_popup_1.text(),
+                    "titre_popup_2" : self.ui.titre_popup_2.text(),
+                    "titre_popup_3" : self.ui.titre_popup_3.text(),
+                    "titre_popup_4" : self.ui.titre_popup_4.text(),
+                },
+                "text" :{
+                    "text_popup_1" : self.ui.text_popup_1.text(),
+                    "text_popup_2" : self.ui.text_popup_2.text(),
+                    "text_popup_3" : self.ui.text_popup_3.text(),
+                    "text_popup_4" : self.ui.text_popup_4.text(),
+                },
+                "timer" :{
+                    "delai_fermeture" : self.ui.delai_fermeture.text(),
+                    "duree_session" : self.ui.duree_session.text(),
+                    "timer_second_popup" : self.ui.timer_second_popup.text(),
+                    "timer_dernier_popup" : self.ui.timer_dernier_popup.text(),
+                },
+                "fiche" :{
+                    "fiche_log" : self.ui.fiche_log.isChecked(),
+                    "fiche_activation" : self.ui.ficher_activation.isChecked(),
+                    "fiche_nom" : self.ui.fiche_nom.isChecked(),
+                    "fiche_mail" : self.ui.fiche_mail.isChecked(),
+                    "fiche_adresse" : self.ui.fiche_adresse.isChecked(),
+                    "fiche_telephone" : self.ui.fiche_tel.isChecked(),
+                    "fiche_duree_session" : self.ui.fiche_duree_session.isChecked(),
+                    "fiche_15min" : self.ui.fiche_15min.isChecked(),
+                    "fiche_30min" : self.ui.fiche_30min.isChecked(),
+                    "fiche_1h" : self.ui.fiche_1h.isChecked(),
+                    "fiche_bouton_reglement" : self.ui.fiche_bouton_reglement.isChecked(),
+                    "fiche_reglement" : self.ui.fiche_reglement.isChecked(),
+                },
+                "session" :{
+                    "session_user" : self.ui.session_user.text(),
+                    "session_activation" : self.ui.session_activation.text(),
+                },
+                "style" :{
+                    "largeur_popup" : self.ui.largeur_popup.text(),
+                    "hauteur_popup" : self.ui.hauteur_popup.text(),
+                    "police" : self.ui.police.text(),
+                    "taille_police" : self.ui.taille_police.text(),
+                },
+                "fermeture" :{
+                    "fermeture_session" : self.ui.fermeture_session.text(),
+                    "fermeture_popup" : self.ui.fermeture_popup.text(),
+                }
+            }
         # Cette fonction est appelée lorsque l'utilisateur clique sur le bouton "Enregistrer"
 
         # Récupérez les données de l'interface utilisateur
