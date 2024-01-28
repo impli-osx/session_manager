8dc479d51893096e1828f5952c548f44af968b8f
impli <38264300+impli-osx@users.noreply.github.com>
2024-01-28 19:43:44 +0100

    update

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ import pandas as pd
 import zipfile
 import openpyxl
 from functools import partial
-from PyQt6.QtWidgets import QApplication, QDialog, QLabel, QPushButton, QVBoxLayout, QSpacerItem, QSizePolicy, QLineEdit, QComboBox, QMessageBox
-from PyQt6.QtGui import QFont
+from PyQt6.QtWidgets import QApplication, QDialog, QLabel, QPushButton, QVBoxLayout, QSpacerItem, QSizePolicy, QLineEdit, QComboBox, QMessageBox, QColorDialog
+from PyQt6.QtGui import QFont, QColor
 from PyQt6.QtCore import QTimer, Qt
 from ficheentree import FicheWindow as FicheEntreeWindow
 from PyQt6.QtWidgets import QApplication
@@ -30,7 +30,9 @@ class test(QDialog):
         fenetre = QDialog()
         fenetre.setFixedSize(int(largeur), int(hauteur))
         fenetre.setWindowTitle(titre)
-        fenetre.setStyleSheet("background-color: white;")
+        fenetre.setStyleSheet("background-color: #5f5f5f;")
+        fenetre.setWindowFlags(Qt.WindowType.FramelessWindowHint)
+        fenetre.setWindowFlags(fenetre.windowFlags() | Qt.WindowType.WindowStaysOnTopHint)
         layout = QVBoxLayout(fenetre)
 
         # Créer un nouveau layout pour le label
@@ -43,6 +45,7 @@ class test(QDialog):
 
         label = QLabel(texte)
         label.setWordWrap(True)
+        label.setStyleSheet("color : #f2f2f2;")
         font = QFont(police, taille_police)
         label.setFont(font)
 
@@ -67,7 +70,7 @@ class test(QDialog):
             bouton.setStyleSheet("""
             QPushButton:enabled {
                 background: linear-gradient(to bottom, #7892c2 5%, #476e9e 100%);
-                background-color: #7892c2;
+                background-color: #5472bd;
                 border-radius: 4px;
                 border: 1px solid #4e6096;
                 color: #ffffff;
@@ -99,10 +102,16 @@ class test(QDialog):
         #print("Popup créée.") # Pour débugger
         
         
+def get_color():
+    color = QColorDialog.getColor()
+    if color.isValid():
+        print(color.name())
+        
 window = test()
 texte = "Bonjour, je suis un popup qui a beaucoup de choses à dire afin de tester la mise en page et les marges de la fenêtre !"
 largeur = 500
 hauteur = 300
 window.creation_popup("Titre", texte, largeur, hauteur, "Arial", 14, 30)
+get_color()
 # Démarrer la boucle d'événements
 sys.exit(app.exec())
